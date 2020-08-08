e979cd981beaec2d0666a25bafc163270e44a643
Shaun Saker <sakershaun@gmail.com>
2020-08-08 15:16:08 +0200

    move colors

--- src/colors/index.ts
+++ src/colors/index.ts
@@ -3,7 +3,7 @@ export const colors = {
   white: '#FCFCFD',
   grey: '#A2A4A7',
   green: '#029F8F',
-  lightGreen: 'rgba(2,159,143,0.3)',
+  lightGreen: 'rgba(2,159,143,0.1)',
   yellow: '#9C9F02',
   red: '#D1282B',
 };
