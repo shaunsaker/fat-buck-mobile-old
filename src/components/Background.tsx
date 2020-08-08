bb180e2c6d94e8afbb623056207f044fd55422d5
Shaun Saker <sakershaun@gmail.com>
2020-08-08 15:16:16 +0200

    add Background

---
+++ src/components/Background.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from 'react';
+import styled from 'styled-components/native';
+import LinearGradient from 'react-native-linear-gradient';
+import { colors } from '../colors';
+
+const BackgroundContainer = styled(LinearGradient)`
+  flex: 1;
+`;
+
+interface BackgroundProps {
+  children: ReactNode;
+}
+
+export const Background = ({ children }: BackgroundProps) => {
+  return (
+    <BackgroundContainer
+      start={{ x: 0, y: 0 }}
+      end={{ x: 0, y: 1 }}
+      colors={[colors.black, colors.green]}>
+      {children}
+    </BackgroundContainer>
+  );
+};
