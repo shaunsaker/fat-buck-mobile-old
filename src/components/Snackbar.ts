d7dac894564ac957f05b63fdf7ac02f4f28621ee
Shaun Saker <sakershaun@gmail.com>
2020-08-10 07:54:47 +0200

    add Snackbar

---
+++ src/components/Snackbar.ts
@@ -0,0 +1,14 @@
+import RNSnackbar from 'react-native-snackbar';
+import { colors } from '../colors';
+
+export const Snackbar = {
+  show: (text: string) => {
+    RNSnackbar.show({
+      textColor: colors.white,
+      backgroundColor: colors.black,
+      fontFamily: 'Recursive-Bold',
+      duration: 4000,
+      text,
+    });
+  },
+};
